94015d9a0f66586c2756ad291b5d4acf0c00cdb6
AidCheng <cn.aiden.cheng@gmail.com>
2024-01-30 16:50:57 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aiden Cheng on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
